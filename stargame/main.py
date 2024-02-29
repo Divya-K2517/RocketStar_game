521c23e63b74b365ddba1cb46e863ec1cc2a7777
Divya-K2517 <115424525+Divya-K2517@users.noreply.github.com>
2024-02-28 22:24:09 -0500

    Update main.py

--- stargame/main.py
+++ stargame/main.py
@@ -1,5 +1,3 @@
-#apple = star
-#player = rocket
 
 import pygame
 import sys
@@ -9,7 +7,7 @@ pygame.init() #starts up pygame
 screen = pygame.display.set_mode((350, 600)) #creates a screen of width 350 and length 600
 clock = pygame.time.Clock() #sets up an FPS(frames per second)
 
-class Apple: 
+class Star: 
     def __init__(self, image, position, speed):
         self.image = image 
         self.rect = self.image.get_rect(topleft = position)
@@ -24,26 +22,23 @@ score = 0
 TILESIZE = 32
 
 
-
-
-
 #floor
 floor_image = pygame.image.load('stargame/assets/floor.png').convert_alpha()
 floor_image = pygame.transform.scale(floor_image, (TILESIZE*15, TILESIZE*5))
 floor_rect = floor_image.get_rect(bottomleft = (0, screen.get_height()))
 
-# player
-player_image = pygame.image.load('stargame/assets/player_static.png').convert_alpha() #convert alpha line optimizes the image in pygame
-player_image = pygame.transform.scale(player_image, (TILESIZE, TILESIZE*(1.356467))) #paratheses is a tuple w the size of the player, in thise case tile size is a constant
-player_rect = player_image.get_rect(center = (screen.get_width()/2, screen.get_height() - floor_image.get_height() - (player_image.get_height()/2))) #places the player at the center. To make center it does the height of the screen minus the height of the floor, minus half the height of the player
+# rocket
+rocket_image = pygame.image.load('stargame/assets/rocket_static.png').convert_alpha() #convert alpha line optimizes the image in pygame
+rocket_image = pygame.transform.scale(rocket_image, (TILESIZE, TILESIZE*(1.356467))) #paratheses is a tuple w the size of the rocket, in thise case tile size is a constant
+rocket_rect = rocket_image.get_rect(center = (screen.get_width()/2, screen.get_height() - floor_image.get_height() - (rocket_image.get_height()/2))) #places the rocket at the center. To make center it does the height of the screen minus the height of the floor, minus half the height of the rocket
 
-#apple
-apple_image = pygame.image.load('stargame/assets/apple.png').convert_alpha()
-apple_image = pygame.transform.scale(apple_image, (TILESIZE, TILESIZE))
+#star
+star_image = pygame.image.load('stargame/assets/star.png').convert_alpha()
+star_image = pygame.transform.scale(star_image, (TILESIZE, TILESIZE))
 
-apples = [
-    Apple(apple_image, (100,0), 3),
-    Apple(apple_image, (300,0), 3)
+stars = [
+    Star(star_image, (100,0), 3),
+    Star(star_image, (300,0), 3)
 ]
 
 #fonts
@@ -62,30 +57,30 @@ def update():
     keys = pygame.key.get_pressed() #checks if keys r being pressed
 
     if keys[pygame.K_LEFT]:
-        player_rect.x = player_rect.x - 8 #moves the x value of player_rect left by 8
+        rocket_rect.x = rocket_rect.x - 8 #moves the x value of rocket_rect left by 8
     elif keys[pygame.K_RIGHT]:
-        player_rect.x = player_rect.x + 8 #moves the x value of player_rect right by 8
+        rocket_rect.x = rocket_rect.x + 8 #moves the x value of rocket_rect right by 8
 
-    #apple movingx
-    for apple in apples:
-        apple.move() #calling the move method for each apple in apples
-        if apple.rect.colliderect(floor_rect): #apple will go away if it hits the ground and a new one will appear
-            apples.remove(apple)
-            apples.append(Apple(apple_image, (random.randint(50,300), -50), speed))
-        elif apple.rect.colliderect(player_rect):
-            apples.remove(apple)
-            apples.append(Apple(apple_image, (random.randint(50,300), -50), speed))
+    #star movingx
+    for star in stars:
+        star.move() #calling the move method for each star in stars
+        if star.rect.colliderect(floor_rect): #star will go away if it hits the ground and a new one will appear
+            stars.remove(star)
+            stars.append(Star(star_image, (random.randint(50,300), -50), speed))
+        elif star.rect.colliderect(rocket_rect):
+            stars.remove(star)
+            stars.append(Star(star_image, (random.randint(50,300), -50), speed))
             speed = speed + 0.1
             score = score + 1
             pickup.play()
 
 def draw(): #gives screen a color/background
     screen.fill("darkblue")
-    screen.blit(player_image, player_rect) #numnbers in parenthesis puts the player at a spot on the screen
+    screen.blit(rocket_image, rocket_rect) #numnbers in parenthesis puts the rocket at a spot on the screen
     screen.blit(floor_image, floor_rect) #puts the floor on the screen, rect uses the x and y value of the floor_rect instead of a hard coded value
 
-    for apple in apples:
-        screen.blit(apple.image, apple.rect) #for each apple in apples list, it draws its unique image and position
+    for star in stars:
+        screen.blit(star.image, star.rect) #for each star in stars list, it draws its unique image and position
     score_text = font.render(f'Score: {score}', True, "white") #inside the brackets u can put a variable
     screen.blit(score_text, (5,5))
 
